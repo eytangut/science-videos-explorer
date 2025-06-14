0c7b6ca0ade306c75a6eaa871472c35273abf7f5
איתן גוטרייך <eytangut@gmail.com>
2025-06-14 18:14:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank or can be deleted.
-// The configuration has been consolidated into next.config.ts to resolve startup errors.
-// If this file is not automatically deleted by the system, you can safely remove it.
